fe812976c38e8cbbe9e7f5865a663ad0fa1a5dbc
jakub <ntonek32@gmail.com>
2024-11-28 21:42:00 +0100

    Usunięcie wypisywań debugowania

--- Merge_Sort_Tests/src/merge_sort.cpp
+++ Merge_Sort_Tests/src/merge_sort.cpp
@@ -19,12 +19,10 @@ std::tuple<int*, int> merge_sort::_merge_arrays( std::tuple<int*, int> left, std
 
 std::tuple<int*, int> merge_sort::_merge_arrays(int* array_left, int size_array_left, int* array_right, int size_array_right) {
     int* new_array = new int[size_array_left + size_array_right];
-    std::cout << "Size" << size_array_left  << " E " << size_array_right << std::endl;
-
+   
     int index_left = 0;
     int index_right = 0;
     for( int index = 0; index < size_array_left + size_array_right; index++ ) {
-        std::cout << "TRT " << array_left[index_left] << " : " << array_right[index_right] << std::endl;
         if( (array_left[ index_left ] < array_right[ index_right ] && index_left < size_array_left) || index_right >= size_array_right ) {
             new_array[ index ] = array_left[ index_left ];
             index_left++;
@@ -35,8 +33,8 @@ std::tuple<int*, int> merge_sort::_merge_arrays(int* array_left, int size_array_
         }
     }
     
-    // delete[] array_left;
-    // delete[] array_right;
+    delete[] array_left;
+    delete[] array_right;
     return {new_array, size_array_left + size_array_right};
 }
 
@@ -44,7 +42,6 @@ std::tuple<int*, int> merge_sort::_merge_arrays(int* array_left, int size_array_
 std::tuple<int*, int> merge_sort::sort( int* array_to_sort, int size_array_to_sort ) {
     if (size_array_to_sort <= 2) {
         if( size_array_to_sort == 1 ) {
-            std::cout << "Jesen" << array_to_sort[0] << std::endl;
             return {array_to_sort, size_array_to_sort};
         }
         else if ( size_array_to_sort == 2 ) {
@@ -53,7 +50,6 @@ std::tuple<int*, int> merge_sort::sort( int* array_to_sort, int size_array_to_so
                 array_to_sort[0] = array_to_sort[1];
                 array_to_sort[1] = temporary;
             }
-            std::cout << "swa" << array_to_sort[0] << " " << array_to_sort[1] << std::endl;
             return {array_to_sort, size_array_to_sort};
         }
     }
@@ -76,7 +72,5 @@ std::tuple<int*, int> merge_sort::sort( int* array_to_sort, int size_array_to_so
             array_right[(index - size_array_left)] = array_to_sort[index];
         }
     }
-    // delete[] right_side;
-    // delete[] array_left;
     return this->_merge_arrays(  this->sort( array_left, size_array_left ), this->sort( array_right, size_array_right ) );
 }
